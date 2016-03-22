1e915c13d19ac4b2c8994f3253def39e65142635
triplehelix <mikehedden@gmail.com>
2016-03-21 21:01:35 -0700

    made some tweaks to the task_add modal

--- web/controllers/dashboard_controller.js
+++ web/controllers/dashboard_controller.js
@@ -1,6 +1,25 @@
 (function () {
-	angular.module('taskAssistant').controller('DashboardController', ['$scope', function ($scope) {
-		//TODO
+	angular.module('taskAssistant').controller('DashboardController', ['$scope', '$log', '$uibModal', function ($scope, $log, $uibModal) {
+		// Open task Modal
+		$scope.openAddTaskModal = function (){
+			var modalInstance = $uibModal.open({
+				animation: true,
+				templateUrl: 'views/modals/task_add.html',
+				controller: 'TaskModalController',
+				resolve: {}
+			});
+
+			modalInstance.result.then(function (result){
+				// TODO: Result?
+				if(!result.success){
+					$log.error("Failed to submit task because of: " + result.message);
+				}else{
+					$log.log("Task submitted successfully");
+				}
+			}, function () {
+				$log.info("Task Modal closed with no submission.");
+			});
+		};
 			
 	}]);
 })();
