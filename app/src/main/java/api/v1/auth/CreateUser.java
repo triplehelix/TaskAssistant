e5dd776f767772129ef9fea58436115322dba88f
Ken Lyon <lyonk6@unlv.nevada.edu>
2016-07-13 21:10:52 -0700

    Removed tab chars from the CreateUser api.

--- app/src/main/java/api/v1/auth/CreateUser.java
+++ app/src/main/java/api/v1/auth/CreateUser.java
@@ -25,54 +25,53 @@ import api.v1.model.User;
  */
 @WebServlet("/api/v1/auth/CreateUser")
 public class CreateUser extends AuthRequestHandler{
-	/**
-	 * POST
-	 * request 
-	 *   email
-	 *   password
-	 * response
-	 *   success
-	 *   error
-	 * 
-	 * @param request
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	public void doPost(HttpServletRequest request, 
-			HttpServletResponse response)throws ServletException, IOException {
-		//First get the email and password.
-		boolean error=false;
-		String errorMsg = "no error";
-		User user=new User();
-		int errorCode = 0;
+    /**
+     * POST
+     * request 
+     *   email
+     *   password
+     * response
+     *   success
+     *   error
+     * 
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void doPost(HttpServletRequest request, 
+            HttpServletResponse response)throws ServletException, IOException {
+        //First get the email and password.
+        boolean error=false;
+        String errorMsg = "no error";
+        User user=new User();
+        int errorCode = 0;
         JSONObject jsonRequest = new JSONObject();
-		try{
-			jsonRequest=parseRequest(request.getParameter("params"));
+        try{
+            jsonRequest=parseRequest(request.getParameter("params"));
             String email= parseJsonAsEmail((String)jsonRequest.get("email"));
-			String password=parseJsonAsPassword((String)jsonRequest.get("password"));
-			user.setEmail(email);
-			user.setPassword(password);
-			
+            String password=parseJsonAsPassword((String)jsonRequest.get("password"));
+            user.setEmail(email);
+            user.setPassword(password);
             userRepository.add(user);
-		}catch(BusinessException b) {
+        }catch(BusinessException b) {
             log.error("An error occurred while handling a CreateUser Request: {}.", jsonRequest.toJSONString(), b);
             errorMsg = "Error. " + b.getMessage();
             errorCode = b.getError().getCode();
             error = true;
-		}catch(SystemException s){
+        }catch(SystemException s){
             log.error("An error occurred while handling a CreateUser Request: {}.", jsonRequest.toJSONString(), s);
             errorMsg = "Error. " + s.getMessage();
             errorCode = s.getError().getCode();
             error = true;
-		}
+        }
 
-		JSONObject jsonResponse = new JSONObject();
-		if (error){
-			jsonResponse.put("error", ErrorHelper.createErrorJson(errorCode, errorMsg));
-		}else {
-			jsonResponse.put("success", true);
-		}
-		sendMessage(jsonResponse, response);
-	}
+        JSONObject jsonResponse = new JSONObject();
+        if (error){
+            jsonResponse.put("error", ErrorHelper.createErrorJson(errorCode, errorMsg));
+        }else {
+            jsonResponse.put("success", true);
+        }
+        sendMessage(jsonResponse, response);
+    }
 }
