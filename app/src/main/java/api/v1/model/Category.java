db2c383377b3078dc9ee1cda6d869e2b51693319
Ken Lyon <lyonk6@unlv.nevada.edu>
2016-08-27 16:23:40 -0700

    Added hashcode, equals and a copy constructor to the Category model.

--- app/src/main/java/api/v1/model/Category.java
+++ app/src/main/java/api/v1/model/Category.java
@@ -2,6 +2,7 @@ package api.v1.model;
 
 import api.v1.error.BusinessException;
 import api.v1.error.Error;
+import api.v1.helper.ModelHelper;
 import com.google.appengine.repackaged.com.google.gson.Gson;
 
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ public class Category {
 	private String name;
 	private String description;
     private ArrayList<Integer> taskIds;
+    private ArrayList<Integer> scheduleIds;
 
 
 	/**
@@ -36,19 +38,25 @@ public class Category {
     public void setName(String name)throws BusinessException {
         if(name==null || name.equals(""))
             throw new BusinessException("The Category name cannot be empty.", Error.valueOf("INVALID_NAME_ERROR"));
-        this.name=name;
+        this.name=name.trim();
     }
     public String getDescription() {
 		return description;
 	}
 	public void setDescription(String description) {
-		this.description = description;
+		this.description = description.trim();
 	}
     public ArrayList<Integer> getTaskIds() {
         return taskIds;
     }
     public void setTaskIds(ArrayList<Integer> taskIds) {
         this.taskIds = taskIds;
+    }
+    public ArrayList<Integer> getScheduleIds() {
+        return scheduleIds;
+    }
+    public void setScheduleIds(ArrayList<Integer> scheduleIds) {
+        this.scheduleIds = scheduleIds;
     }
     public int getUserId() {
         return userId;
@@ -65,5 +73,43 @@ public class Category {
     public String toJson(){
         Gson gson=new Gson();
         return gson.toJson(this);
+    }
+    /**
+     * Creates and returns a deep copy of this object.
+     */
+    public Category(Category originalCategory){
+        this.id=originalCategory.getId();
+        this.userId=originalCategory.getUserId();
+        this.name= new String(originalCategory.getName());
+        this.description=new String(originalCategory.getDescription());
+        this.taskIds= ModelHelper.copyIntegerArrayList(originalCategory.getTaskIds());
+        this.scheduleIds= ModelHelper.copyIntegerArrayList(originalCategory.getScheduleIds());
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        if (id != category.id) return false;
+        if (userId != category.userId) return false;
+        if (!name.equals(category.name)) return false;
+        if (description != null ? !description.equals(category.description) : category.description != null)
+            return false;
+        if (taskIds != null ? !taskIds.equals(category.taskIds) : category.taskIds != null) return false;
+        return scheduleIds != null ? scheduleIds.equals(category.scheduleIds) : category.scheduleIds == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + userId;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (taskIds != null ? taskIds.hashCode() : 0);
+        result = 31 * result + (scheduleIds != null ? scheduleIds.hashCode() : 0);
+        return result;
     }
 }
