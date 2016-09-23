65402a8349ca092a7b0414d29d13c43b47a49f98
Ken Lyon <lyonk6@unlv.nevada.edu>
2016-09-23 10:25:28 -0700

    Removed

--- app/src/main/java/api/v1/AuthRequestHandler.java
+++ app/src/main/java/api/v1/AuthRequestHandler.java
@@ -27,7 +27,7 @@ public class AuthRequestHandler extends BaseRequestHandler{
      * @param email
      * @throws BusinessException
      */
-	protected void verifyEmailIsValid(String email) throws BusinessException{
+    protected void verifyEmailIsValid(String email) throws BusinessException{
         try {
             InternetAddress emailAddr = new InternetAddress(email);
             emailAddr.validate();
@@ -58,82 +58,82 @@ public class AuthRequestHandler extends BaseRequestHandler{
         }
     }
 
-	/**
-	 * Verify that each taskId supplied belongs to a TaskList that belongs to the
-	 * supplied User id.
-	 * @param userId
-	 * @param taskIds
-	 */
-	protected void verifyTaskPrivileges(int userId, ArrayList<Integer> taskIds)
-			throws BusinessException, SystemException {
-		if(taskIds==null)
-			return;
-		for(Integer i: taskIds){
-			// First fetch the Task specified in the taskIds list.
-			Task task=new Task();
-			task.setId(i);
-			task=taskRepository.get(task);
+    /**
+     * Verify that each taskId supplied belongs to a TaskList that belongs to the
+     * supplied User id.
+     * @param userId
+     * @param taskIds
+     */
+    protected void verifyTaskPrivileges(int userId, ArrayList<Integer> taskIds)
+            throws BusinessException, SystemException {
+        if(taskIds==null)
+            return;
+        for(Integer i: taskIds){
+            // First fetch the Task specified in the taskIds list.
+            Task task=new Task();
+            task.setId(i);
+            task=taskRepository.get(task);
 
-			//Next fetch the TaskList that owns this Task.
-			TaskList taskList=new TaskList();
-			taskList.setId(task.getTaskListId());
-			taskList=taskListRepository.get(taskList);
+            //Next fetch the TaskList that owns this Task.
+            TaskList taskList=new TaskList();
+            taskList.setId(task.getTaskListId());
+            taskList=taskListRepository.get(taskList);
 
-			LOGGER.debug("TaskList owner id: " + taskList.getUserId());
-			LOGGER.debug("User id: " + userId);
-			//Finally, verify that ownership of the TaskList.
-			if(taskList.getUserId()==userId)
-				return;
-			else{
-				String message= "This task cannot be accessed by the specified user. ";
-				throwObjectOwnershipError(userId, message);
-			}
-		}
-	}
+            LOGGER.debug("TaskList owner id: " + taskList.getUserId());
+            LOGGER.debug("User id: " + userId);
+            //Finally, verify that ownership of the TaskList.
+            if(taskList.getUserId()==userId)
+                return;
+            else{
+                String message= "This task cannot be accessed by the specified user. ";
+                throwObjectOwnershipError(userId, message);
+            }
+        }
+    }
 
-	protected void throwObjectOwnershipError(int userId, String message) throws BusinessException, SystemException{
-		User user = new User();
-		user.setId(userId);
-		user=userRepository.get(user);
-		message+=" {id: " + userId +", email: " + user.getEmail() + "}";
-		throw new BusinessException(message, Error.valueOf("OBJECT_OWNERSHIP_ERROR"));
-	}
+    protected void throwObjectOwnershipError(int userId, String message) throws BusinessException, SystemException{
+        User user = new User();
+        user.setId(userId);
+        user=userRepository.get(user);
+        message+=" {id: " + userId +", email: " + user.getEmail() + "}";
+        throw new BusinessException(message, Error.valueOf("OBJECT_OWNERSHIP_ERROR"));
+    }
 
-	/**
-	 * Verify that the User with the specified ID has permission to access these
-	 * schedules.
+    /**
+     * Verify that the User with the specified ID has permission to access these
+     * schedules.
      * TODO This method is broken just like the verifyCategoryPrivleges was!
-	 * @param userId
-	 * @param scheduleIds
-	 */
-	protected void verifySchedulePrivileges(int userId, ArrayList<Integer> scheduleIds) throws BusinessException, SystemException{
-		if(scheduleIds==null || scheduleIds.size()==0)
-			return;
-		Schedule schedule=new Schedule();
-		for(int i: scheduleIds)
-			schedule.setId(i);
-		schedule=scheduleRepository.get(schedule);
-		if (schedule.getUserId()==userId)
-			return;
-		else{
-			String message= "This schedule cannot be accessed by the specified user. ";
-			throwObjectOwnershipError(userId, message);
-		}
-	}
+     * @param userId
+     * @param scheduleIds
+     */
+    protected void verifySchedulePrivileges(int userId, ArrayList<Integer> scheduleIds) throws BusinessException, SystemException{
+        if(scheduleIds==null || scheduleIds.size()==0)
+            return;
+        Schedule schedule=new Schedule();
+        for(int i: scheduleIds)
+            schedule.setId(i);
+        schedule=scheduleRepository.get(schedule);
+        if (schedule.getUserId()==userId)
+            return;
+        else{
+            String message= "This schedule cannot be accessed by the specified user. ";
+            throwObjectOwnershipError(userId, message);
+        }
+    }
 
-	/**
-	 * Verify that the User with the specified ID has permission to access these
-	 * schedules.
+    /**
+     * Verify that the User with the specified ID has permission to access these
+     * schedules.
      *  TODO This method is broken just like the verifySchedulePrivleges was!
-	 * @param userId
-	 * @param categoryIds
-	 */
-	protected void verifyCategoryPrivileges(int userId, ArrayList<Integer> categoryIds) throws BusinessException, SystemException{
-		LOGGER.info("Here is the user and the categories we are testing the validation of {} {} ", userId, new Gson().toJson(categoryIds));
-	    if(categoryIds==null)
-			return;
-		Category category=new Category();
-		for(int i: categoryIds)
+     * @param userId
+     * @param categoryIds
+     */
+    protected void verifyCategoryPrivileges(int userId, ArrayList<Integer> categoryIds) throws BusinessException, SystemException{
+        LOGGER.info("Here is the user and the categories we are testing the validation of {} {} ", userId, new Gson().toJson(categoryIds));
+        if(categoryIds==null)
+            return;
+        Category category=new Category();
+        for(int i: categoryIds)
             category.setId(i);
         category = categoryRepository.get(category);
         if (category.getUserId() == userId)
@@ -142,5 +142,5 @@ public class AuthRequestHandler extends BaseRequestHandler{
             String message = "This category cannot be accessed by the specified user. ";
             throwObjectOwnershipError(userId, message);
         }
-	}
+    }
 }
